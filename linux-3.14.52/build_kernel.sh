03195529c7e186cda43ac193f96cbb89a28bd6a3
Depeng Shao <c_dshao@qti.qualcomm.com>
2016-03-09 10:05:56 +0800

    kernel: rename config file
    Signed-off-by: Depeng Shao <c_dshao@qti.qualcomm.com>

--- linux-3.14.52/build_kernel.sh
+++ linux-3.14.52/build_kernel.sh
@@ -3,17 +3,17 @@
 echo
 echo "Make zImage"
 echo
-make myimx6_defconfig
+make jzt_uhf_defconfig
 make zImage -j16
 cp arch/arm/boot/zImage ../$IMAGE/zImage-myimx6
 
 echo
 echo "Make dtb"
 echo
-make myimx6ek200-6q.dtb
+make jzt-uhf-6q.dtb
 #make myimx6ek200-6s.dtb
 
-cp arch/arm/boot/dts/myimx6ek*.dtb ../$IMAGE/
+cp arch/arm/boot/dts/jzt-uhf-6q.dtb ../$IMAGE/
 
 echo
 echo "Make modules"
